505e385279472ae35eee7d2ce944a51cc5322985
dexhrestha <dexter.shrestha10@gmail.com>
2018-06-24 10:40:43 +0545

    Final

--- Procfile
+++ Procfile
@@ -1,2 +1,2 @@
 web: gunicorn app:app 
-clock: python detectChange.py 
+clock: python clo.py 
--- clo.py
+++ clo.py
@@ -1,16 +1,1 @@
-from apscheduler.schedulers.blocking import BlockingScheduler
-
-sleep_time = 1
-sched = BlockingScheduler()
-
-@sched.scheduled_job('interval',minutes=int(open('time.txt','r').read()))
-def time_job():
-	
-	# bot.send_text_message('1928179273867668','Sleep time:'+str(sleep_time))
-	print('Run every'+str(minutes)+'minutes')
-	# detectChange()
-	sleep_time = 3
-
-sched.start()
-
-
+print("easdas;dfljaksldfj;asdlf")
